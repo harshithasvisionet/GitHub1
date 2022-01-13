975fa190baa655210a3cf0fea3ecffa884bb988e
Harshitha.S <Harshitha.S@VSPLBLRLT0766>
2022-01-13 16:51:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitHub1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            if (i == 4)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
